6c298626b05c6d9d7b6596b471a8c29207998b7e
Vithika Jha <vithikajha14@gmail.com>
2020-08-22 09:56:30 +0530

    setup styling

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import 'materialize-css/sass/materialize.scss'
+import '@/assets/css/override.css'
+import '@/assets/css/style.css'
+
 Vue.config.productionTip = false;
 
 new Vue({
