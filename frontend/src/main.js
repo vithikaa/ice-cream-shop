a5267dc8714b5a149ce4c2e365efffd51ffb22b6
Vithika Jha <vithikajha14@gmail.com>
2020-08-22 09:58:03 +0530

    lint em all

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
 import 'materialize-css/sass/materialize.scss'
 import '@/assets/css/override.css'
 import '@/assets/css/style.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
