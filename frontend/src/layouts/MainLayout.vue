3c68aae09f5210f9c73ff7d4e8cd7adc8cecd082
Vithika Jha <vithikajha14@gmail.com>
2020-08-22 11:36:37 +0530

    create layout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,5 +1,53 @@
 <template>
-  <router-view />
+  <div class="main-layout">
+    <header>
+      <nav class="white" role="navigation">
+        <div class="nav-wrapper container">
+          <a id="logo-container" href="#" class="brand-logo">
+            <!-- Vithika's  -->Ice Cream
+          </a>
+          <ul class="right hide-on-med-and-down">
+            <li>
+              <router-link to="/home">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/about">About</router-link>
+            </li>
+
+            <li>
+              <a @click="() => {}">
+                <i class="material-icons left">shopping_cart</i>
+                Cart
+              </a>
+            </li>
+          </ul>
+
+          <ul id="nav-mobile" class="sidenav">
+            <li>
+              <router-link to="/home">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/about">About</router-link>
+            </li>
+
+            <li>
+              <a @click="() => {}">
+                <i class="material-icons left">shopping_cart</i>
+                Cart
+              </a>
+            </li>
+          </ul>
+
+          <a href="#" data-target="nav-mobile" class="sidenav-trigger">
+            <i class="material-icons">menu</i>
+          </a>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script>
