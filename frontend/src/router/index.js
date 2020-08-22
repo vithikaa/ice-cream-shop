ca5f070576fb824fbdf3db7ab83d7428e6c45a46
Vithika Jha <vithikajha14@gmail.com>
2020-08-22 11:28:58 +0530

    cleanup, layout and route setup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('@/layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        redirect: '/home'
+      },
+      {
+        path: 'home',
+        name: 'Home',
+        component: () => import('@/views/Home.vue')
+      },
+      {
+        path: 'about',
+        name: 'About',
+        component: () => import('@/views/About.vue')
+      }
+    ]
   }
 ]
 
