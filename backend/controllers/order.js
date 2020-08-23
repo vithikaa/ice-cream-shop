63a10bfc334677c3d3d0211677ec9ad42d58d863
Vithika Jha <vithikajha14@gmail.com>
2020-08-23 09:21:59 +0530

    implement order placement

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,4 +1,7 @@
-const Order = require("../models/order").model;
+const Order = require('../models/order').model
+const Wafer = require('../models/wafer').model
+const Flavor = require('../models/flavor').model
+const Topping = require('../models/topping').model
 
 exports.orders_get = async (req, res) => {
   Order.find({})
@@ -6,52 +9,82 @@ exports.orders_get = async (req, res) => {
       res.send({
         success: true,
         data: docs,
-      });
+      })
     })
     .catch((err) => {
-      console.error(err);
+      console.error(err)
       res.status(500).send({
         success: false,
         error: err,
-      });
-    });
-};
+      })
+    })
+}
 
 exports.order_get = async (req, res) => {
-  const { orderId } = req.params;
+  const { orderId } = req.params
   Order.findOne({ _id: orderId })
     .then((doc) => {
       res.send({
         success: true,
         data: doc,
-      });
+      })
     })
     .catch((err) => {
-      console.error(err);
+      console.error(err)
       res.status(500).send({
         success: false,
         error: err,
-      });
-    });
-};
+      })
+    })
+}
 
 exports.order_post = async (req, res) => {
-  const { customer, icecreams } = req.body;
+  const { customer, icecreams } = req.body
+
+  // calculate price
+  await Promise.all(
+    icecreams.map(async (icecream) => {
+      console.log(icecream)
+      let flavor = (
+        await Flavor.findOne({ _id: icecream.flavor }).select('price').lean()
+      ).price
+
+      let wafer = (
+        await Wafer.findOne({ _id: icecream.wafer }).select('price').lean()
+      ).price
+
+      let toppings = (
+        await Topping.find({
+          _id: {
+            $in: [icecream.toppings],
+          },
+        })
+          .select('price')
+          .lean()
+      ).reduce((s, a) => s + a.price, 0)
+
+      icecream.price = flavor + wafer + toppings
+    })
+  ).catch((err) => console.error)
+
+  console.log({ icecreams })
+
   const newOrder = new Order({
     customer,
     icecreams,
     date: new Date(),
-  });
+  })
+
   newOrder
     .save()
     .then((docs) => {
-      res.send(docs);
+      res.send({ success: true, data: docs })
     })
     .catch((err) => {
-      console.error(err);
+      console.error(err)
       res.status(500).send({
         success: false,
         error: err,
-      });
-    });
-};
+      })
+    })
+}
